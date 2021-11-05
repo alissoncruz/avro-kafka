e668c6fd220fdf0018e450214bd2e0a719e98dbb
Jeremias-2000 <jdossantospinheiro20@gmail.com>
2021-11-05 11:04:14 -0300

    second commit

--- src/main/java/com/example/kafkaavro/config/KafkaProducerConfig.java
+++ src/main/java/com/example/kafkaavro/config/KafkaProducerConfig.java
@@ -3,43 +3,50 @@ package com.example.kafkaavro.config;
 import java.util.HashMap;
 import java.util.Map;
 
+
+import avro.com.example.kafkaavro.dto.person;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.core.ProducerFactory;
-//
-//import com.purnima.jain.avro.dto.PersonDto;
-//
-//import io.confluent.kafka.serializers.KafkaAvroSerializer;
-//import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
 
 @Configuration
+
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
 
-    @Value("${schema.registry.url}")
-    private String schemaRegistryUrl;
+    /*@Value("${spring.kafka.bootstrap-servers}" )
+    private String bootstrapServers;*/
+
+    /*@Value("${schema.registry.url}")
+    private String schemaRegistryUrl;*/
 
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        //props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-       // props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+
+
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
         return props;
     }
 
-//    @Bean
-//    public ProducerFactory<String, PersonDto> producerFactory() {
-//        return new DefaultKafkaProducerFactory<>(producerConfigs());
-//    }
+    /*@Bean
+    public ProducerFactory<String, person> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+   }*/
 //
 //    @Bean
 //    public KafkaTemplate<String, PersonDto> kafkaTemplate() {
